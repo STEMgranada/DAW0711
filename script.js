b680779fc87f1fa0c22f567e60d6570401b6dae7
PascualDC <pascualdaroca678.alu@stemgranada.com>
2024-11-07 08:42:16 +0100

    Edicion de Js en nuestro proyecto

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-Esto es el Java Script
+function submitForm(event) {
+    event.preventDefault();
+
+    // Simula una respuesta de envío
+    document.getElementById('feedback').classList.remove('hidden');
+
+    // Limpia el formulario después de un segundo
+    setTimeout(() => {
+        document.getElementById('contactForm').reset();
+        document.getElementById('feedback').classList.add('hidden');
+    }, 2000);
+}
